30c93f0d7174ff90961fb575708a896b5b6e8f3f
renxianglong <1057321186@qq.com>
2020-10-23 18:11:55 +0800

    多线程

--- src/main/java/com/project/thread/处理线程返回值/Method4.java
+++ src/main/java/com/project/thread/处理线程返回值/Method4.java
@@ -1,27 +1,37 @@
 package com.project.thread.处理线程返回值;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 //使用线程池
-public class Method4 {
+public class Method4 implements Callable {
 	
 	public static void main(String[] args) {
 		//创建线程池对象
 		ExecutorService newCachedThreadPool=Executors.newCachedThreadPool();
 		//将Callable子线程对象传递给线程池，使用Future来接收
-		Future<String> future=newCachedThreadPool.submit(new Method3());
+		Future<String> future=newCachedThreadPool.submit(new Method4());
 		if(!future.isDone()){
-			System.out.println("task has not finished,please wait");
+			System.out.println("任务 has not finished,please 等待");
 		}
 		try {
-			System.out.println("task return:"+future.get());//通过Future的get()方法获取返回值
+			System.out.println("task return:" + future.get());//通过Future的get()方法获取返回值
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (ExecutionException e) {
 			e.printStackTrace();
 		}
 	}
+
+
+	public static String print(){
+		return "嘻嘻嘻";
+	}
+
+	@Override
+	public String call() throws Exception {
+		return "嘻-嘻";
+	}
+
+
+
 }
